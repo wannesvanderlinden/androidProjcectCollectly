32a40668b4861168551f9759bcafe0aa37cd427a
Wannes Vanderlinden <wannesvanderlindenstudent.ehb.be>
2023-01-02 17:00:56 +0100

    feat: added intro screen + audio for the intro

---
+++ app/src/main/java/com/example/androidprojcectcollectly/SplashActivity.kt
@@ -0,0 +1,48 @@
+package com.example.androidprojcectcollectly
+
+import android.content.Intent
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.graphics.alpha
+import kotlin.time.Duration.Companion.seconds
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        var mediaplayer = MediaPlayer.create(this, R.raw.intro_music)
+        mediaplayer.start()
+        //Animations setting
+        var topAnim = AnimationUtils.loadAnimation(this, R.transition.from_top)
+        var bottomAnim = AnimationUtils.loadAnimation(this, R.transition.buttom_animation)
+        var rotateAnim = AnimationUtils.loadAnimation(this, R.transition.rotate_animation)
+
+        var logo = findViewById<ImageView>(R.id.gameController)
+        var slogan = findViewById<TextView>(R.id.slogan)
+        var createdBy = findViewById<TextView>(R.id.created)
+
+
+        slogan.setAnimation(bottomAnim);
+        createdBy.setAnimation(bottomAnim)
+        logo.setAnimation(rotateAnim)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            mediaplayer.pause()
+            mediaplayer.release()
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            finish()
+
+
+        }, 6000)
+
+    }
+}
